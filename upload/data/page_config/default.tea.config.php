866e1732e7af636e6a1743aae14c716a8a03e6f3
Jwwwtuolaji <hebei_jjc@163.com>
2017-01-06 10:19:55 +0800

    jia2

---
+++ upload/data/page_config/default.tea.config.php
@@ -0,0 +1,74 @@
+<?php
+
+return array (
+  'widgets' => 
+  array (
+    '_widget_275' => 
+    array (
+      'name' => 'four_image_ads',
+      'options' => 
+      array (
+        'ad1_image_url' => '',
+        'ad1_link_url' => '',
+        'ad2_image_url' => 'data/files/mall/template/201701041744478661.jpg',
+        'ad2_link_url' => '',
+        'ad3_image_url' => 'data/files/mall/template/201701041744476238.jpg',
+        'ad3_link_url' => '',
+        'ad4_image_url' => 'data/files/mall/template/201701041744475296.jpg',
+        'ad4_link_url' => '',
+        'ad5_image_url' => 'data/files/mall/template/201701041744473211.jpg',
+        'ad5_link_url' => '',
+      ),
+    ),
+  ),
+  'config' => 
+  array (
+    'cycle_image' => 
+    array (
+      0 => '_widget_275',
+    ),
+  ),
+  'tmp' => 
+  array (
+    '_widget_587' => 
+    array (
+      'options' => 
+      array (
+        0 => 
+        array (
+          'ad_image_url' => 'data/files/mall/template/201701050941019482.jpg',
+          'ad_link_url' => '',
+        ),
+        1 => 
+        array (
+          'ad_image_url' => 'data/files/mall/template/201701050941204527.jpg',
+          'ad_link_url' => '',
+        ),
+      ),
+    ),
+    '_widget_437' => 
+    array (
+      'options' => 
+      array (
+        0 => 
+        array (
+          'ad_image_url' => 'data/files/mall/template/201701051005464286.jpg',
+          'ad_link_url' => '',
+        ),
+      ),
+    ),
+    '_widget_981' => 
+    array (
+      'options' => 
+      array (
+        0 => 
+        array (
+          'ad_image_url' => 'data/files/mall/template/201701051013061578.jpg',
+          'ad_link_url' => '',
+        ),
+      ),
+    ),
+  ),
+);
+
+?>
---
+++ upload/external/widgets/slide_image/widget.info.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'name'      => 'slide_image',
+    'display_name'  => 'slide轮播图',
+    'author'    => 'jjc',
+    'website'   => '',
+    'version'   => '1.0',
+    'desc'      => '循环展示最多6个图片广告，图片广告在后台设置',
+    'configurable'  => true,
+);
+
+?>
