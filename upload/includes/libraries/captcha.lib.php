2647081f27b751052a691ecb8a1bb432e017a5fd
Jwwwtuolaji <hebei_jjc@163.com>
2017-05-22 14:58:27 +0800

    jia

--- upload/includes/libraries/captcha.lib.php
+++ upload/includes/libraries/captcha.lib.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * ECMALL: 验证码类
  * ============================================================================
@@ -15,36 +14,6 @@ if (!defined('IN_ECM'))
 {
     trigger_error('Hacking attempt', E_USER_ERROR);
 }
-
-/**
- * 用例如下
-完整写法
-include_once(ROOT_PATH.'/includes/cls.captcha.new.php');
-$seccode = 'asdf';
-$code = new Captcha();
-$code->code = $seccode;
-$code->width = 150;
-$code->height = 60;
-$code->background = 1;
-$code->adulterate = 1;
-$code->ttf = 1;
-$code->angle = 1;
-$code->color = 1;
-$code->size = 1;
-$code->shadow = 1;
-$code->animator = 0;
-$code->display();
-$code->fontpath = ROOT_PATH.'/includes/captcha/fonts/';
-$code->imagepath = ROOT_PATH.'/includes/captcha/';
-
-简单用法
-include_once(ROOT_PATH.'/includes/cls.captcha.new.php');
-$seccode = 'asdf';
-$code = new Captcha();
-$code->code = $seccode;
-$code->display();
- */
-
 class Captcha
 {
 
@@ -82,7 +51,6 @@ class Captcha
          
         // 画图像
         $im = imagecreatetruecolor($width, $height);
-        dump($im);
         // 定义要用到的颜色
         $back_color = imagecolorallocate($im, 235, 236, 237);
         $boer_color = imagecolorallocate($im, 118, 151, 199);
@@ -92,7 +60,7 @@ class Captcha
         // 画边框
         imagerectangle($im, 0, 0, $width-1, $height-1, $boer_color); 
         // 画干扰线
-        for($i = 0;$i < 5;$i++) {
+        /*for($i = 0;$i < 5;$i++) {
             $font_color = imagecolorallocate($im, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));
             imagearc($im, mt_rand(- $width, $width), mt_rand(- $height, $height), mt_rand(30, $width * 2), mt_rand(20, $height * 2), mt_rand(0, 360), mt_rand(0, 360), $font_color);
         } 
@@ -100,10 +68,9 @@ class Captcha
         for($i = 0;$i < 50;$i++) {
             $font_color = imagecolorallocate($im, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));
             imagesetpixel($im, mt_rand(0, $width), mt_rand(0, $height), $font_color);
-        } 
+        } */
         // 画验证码
         @imagefttext($im, $size , 0, 5, $size + 3, $text_color, 'c:\\WINDOWS\\Fonts\\simsun.ttc', $code);
-        $_SESSION["VerifyCode"]=$code; 
         header("Cache-Control: max-age=1, s-maxage=1, no-cache, must-revalidate");
         header("Content-type: image/png;charset=gb2312");
         imagepng($im);
@@ -133,5 +100,3 @@ function generate_code($len = 4)
 
     return $code;
 }
-
-?>
